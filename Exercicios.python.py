ab9a12e321be4b7e54def5c5a88ddab4f11fc245
Jefferson de Lima Gomes <86693914+Jeffersonlima578@users.noreply.github.com>
2023-10-09 13:35:43 -0300

    Add files via upload

---
+++ Exercicios.python.py
@@ -0,0 +1,17 @@
+# Resolva Este Exercicio de Python pela Proz
+
+
+
+#Uma nova loja de cosméticos abriu no seu bairro e pediram para você elaborar um sistema que imprime na tela na frente da loja os novos produtos que chegaram.
+# O sistema da loja já tem um array com os produtos, você precisa apenas imprimir eles no terminal, um por um.
+#Como desafio opcional, tente imprimir cada produto com a frase "Temos [produto] à venda!" (ex. "Temos máscaras faciais à venda!"). 
+#lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+
+
+
+
+
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores']
+
+for produto in lista_produtos:
+    print("Temos", produto, "à venda!")
