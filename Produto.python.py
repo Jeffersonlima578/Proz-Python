db11f668acde4c1ee9f00165893632deb50accae
Jefferson de Lima Gomes <86693914+Jeffersonlima578@users.noreply.github.com>
2023-10-09 13:24:50 -0300

    Add files via upload

---
+++ Produto.python.py
@@ -0,0 +1,16 @@
+# Exercicio da Proz em Python
+
+#Declare dois arrays, cada um com um mínimo de cinco elementos, e imprima eles no terminal usando o comando print().
+# O primeiro array deve conter os produtos de uma loja da sua escolha (loja de comida, materiais de construção, música, etc).
+# O segundo array deve conter os anos de nascimento de familiares e amigos seus. Lembre-se de usar nomes descritivos para nomear cada variável, 
+# e de usar o tipo de dado apropriado para cada lista (strings, booleanos, números inteiros, floats).
+
+
+
+
+
+produtos_loja = ["Arroz", "Feijão", "Macarrão", "Óleo", "Sal"]
+anos_nascimento = [1990, 1985, 1993, 1980, 2000]
+
+print("Produtos da loja:", produtos_loja)
+print("Anos de nascimento:", anos_nascimento)
