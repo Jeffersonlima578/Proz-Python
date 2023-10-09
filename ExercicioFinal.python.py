ef3256d3a117beb97894c45cacc6a50524d1d3ad
Jefferson de Lima Gomes <86693914+Jeffersonlima578@users.noreply.github.com>
2023-10-09 13:16:43 -0300

    Update ExercicioFinal.python.py

--- ExercicioFinal.python.py
+++ ExercicioFinal.python.py
@@ -1,3 +1,12 @@
+# Exercicio Proz 
+# Resolva este Problema
+#Copie o array apresentado embaixo no seu editor de código, e imprima no terminal: a quantidade de elementos que ele possui, o dado salvo no índice 2, o dado salvo no índice 9, e dado salvo no índice 14.
+#lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'] 
+
+
+
+
+
 
 
 lista_musicos = ['Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
