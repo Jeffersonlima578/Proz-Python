2af153ea64adc23dcd16f2b72c230992b26d939b
Jefferson de Lima Gomes <86693914+Jeffersonlima578@users.noreply.github.com>
2023-10-09 13:07:52 -0300

    Add files via upload

---
+++ ExercicioFinal.python.py
@@ -0,0 +1,8 @@
+
+
+lista_musicos = ['Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+
+print("Quantidade de elementos: ", len(lista_musicos))
+print("Dado no índice 2: ", lista_musicos[2])
+print("Dado no índice 9: ", lista_musicos[9])
+print("Dado no índice 14: ", lista_musicos[14])
